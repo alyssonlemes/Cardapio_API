0a74670857481927dab844302a4d19e6f42b2b90
alyssonlemes <alyssondiamantino@gmail.com>
2024-08-19 12:53:56 -0300

    users_Feature_v1

---
+++ src/main/java/com/example/cardapio/model/entity/user/UserRole.java
@@ -0,0 +1,14 @@
+package com.example.cardapio.model.entity.user;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+    private String role;
+    private UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
