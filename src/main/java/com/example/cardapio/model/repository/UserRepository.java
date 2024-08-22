df5718b92127bbf5e7c4dd63321af1bd572d3705
alyssonlemes <alyssondiamantino@gmail.com>
2024-08-22 12:58:15 -0300

    User_Service feature v1

---
+++ src/main/java/com/example/cardapio/model/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.cardapio.model.repository;
+
+import com.example.cardapio.model.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    //método para consultar usuário por email e retorna um User Details
+     UserDetails findByLogin(String login);
+}
